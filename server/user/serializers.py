0d3900f14b09231180ecd3864ad0f9a294ecdc49
Leonardo <leovictorsr@gmail.com>
2024-09-03 23:51:06 +0300

    add: wallet address to db

--- server/user/serializers.py
+++ server/user/serializers.py
@@ -9,7 +9,7 @@ class RegistrationSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = get_user_model()
-        fields = ("first_name", "last_name", "email", "password", "password2")
+        fields = ("first_name", "last_name", "email", "wallet_address", "password", "password2")
         extra_kwargs = {
             "password": {"write_only": True},
             "password2": {"write_only": True}
@@ -20,8 +20,8 @@ class RegistrationSerializer(serializers.ModelSerializer):
             email=self.validated_data["email"],
             first_name=self.validated_data["first_name"],
             last_name=self.validated_data["last_name"],
+            wallet_address = self.validated_data["wallet_address"]
         )
-
         password = self.validated_data["password"]
         password2 = self.validated_data["password2"]
 
@@ -44,4 +44,4 @@ class LoginSerializer(serializers.Serializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ("id", "email", "is_staff", "first_name", "last_name")
+        fields = ("id", "email", "is_staff", "wallet_address", "first_name", "last_name")
